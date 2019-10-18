4b131a69aca2b0a52df84c7d6be884a5aa64336b
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-18 12:28:07 +0100

    used protected route

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,37 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route,withRouter } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
 import BubblePage from "./components/BubblePage";
 
-function App() {
+function App(appProps) {
   return (
-    <Router>
+
       <div className="App">
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-        <Route
-          exact path="/colors"
-          render={props => {
-            if (localStorage.getItem('payload')) {
-              return <BubblePage {...props} />
-            }
-            return <Redirect to="/api/login" />
-          }}
+       
+         <Route
+          exact
+          path='/colors'
+          render={props => protectedRoute(BubblePage, {...props,...appProps})}
         />
       </div>
-    </Router>
+
   );
 }
 
-export default App;
+function protectedRoute(Component, props) {
+  
+  if (localStorage.getItem('payload')) {
+    return <Component {...props} />;
+  }
+  return <Redirect to='/' />;
+}
+
+
+export default withRouter(App);
